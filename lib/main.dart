03e50080962a50919283d4aa47e2295b392ef7b4
Niang <niangabdoukarim062@gmail.com>
2025-11-26 11:11:39 -0500

    v1.1 - Ajout du compteur et du bouton Incrémenter

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,8 @@ class MyWidget extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<MyWidget> {
+  int compteur = 0; // variable dynamique
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,8 +22,19 @@ class _MyWidgetState extends State<MyWidget> {
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text("Atelier 4 - StatefulWidget"),
+            children: [
+              const Text("Atelier 4 - StatefulWidget"),
+              const SizedBox(height: 20),
+              Text("La valeur du compteur est : $compteur"),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    compteur++;
+                  });
+                },
+                child: const Text("Incrémenter"),
+              ),
             ],
           ),
         ),
